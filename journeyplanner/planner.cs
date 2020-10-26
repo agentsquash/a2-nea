c473f45ad156b0397dc68f263f0592bb1a47b197
Alex Ashley <me@alexashley.xyz>
2020-10-26 13:19:46 +0000

    Update planner.cs

--- journeyplanner/planner.cs
+++ journeyplanner/planner.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace JourneyPlanner
+{
+    class Journey
+    {
+        protected string departureCRS;
+        protected string arrivalCRS;
+        protected DateTime departureTime;
+        public Journey(string departure, string arrival)
+        {
+
+        }
+    }
+}
